950ffb61c9c92c8f22e2fbcd92c21daeac46e4d5
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2022-07-12 23:24:35 +0700

    Finish-pengembangan

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,23 @@
+// Js untuk navbar pada tampilan mobile, burger navbar jika fi click akan menampilkan navbar
 const toggleButton = document.getElementById('toogle');
 const navList = document.getElementById('navi-list');
 
 toggleButton.addEventListener('click', () => {
     navList.classList.toggle('active');
-})
+});
+
+// Js untuk merubah tampilan button menjadi dalam proses
+function rubah() {
+    document.getElementById("button-1").innerHTML = "Dalam Proses";
+};
+
+// Js untuk merubah tampilan button menjadi tahap pengembangan
+function rubah2() {
+    document.getElementById("button-2").innerHTML = "Tahap pengembangan";
+};
+
+// Js untuk button scrol footer untuk kembali ke atas
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
